76f65a1d9019ff437678b183e47229ee6dfbae5e
albalopes <alba.lopes@ifrn.edu.br>
2024-11-19 00:02:07 +0000

    Adicionando funcionalidades do CRUD - Update e Delete

--- controllers/Usuario.py
+++ controllers/Usuario.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, flash
+from flask import render_template, request, redirect, flash, url_for
 from models.Usuario import Usuario
 from utils import db
 from flask import Blueprint
@@ -31,3 +31,43 @@ def recovery(id):
     else:
         usuario = Usuario.query.get(id)
         return render_template('usuarios_detalhes.html', usuario=usuario)
+
+@bp_usuarios.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    if id and request.method=='GET':
+        usuario = Usuario.query.get(id)
+        return render_template('usuarios_update.html', usuario=usuario)
+
+    if request.method=='POST':
+        usuario = Usuario.query.get(id)
+        usuario.nome = request.form.get('nome')
+        usuario.email = request.form.get('email')
+	
+    if (request.form.get('senha') and request.form.get('senha') == request.form.get('csenha')):
+        usuario.senha = request.form.get('senha')
+    else:
+        flash('Senhas não conferem')
+        return redirect(url_for('.update', id=id))
+
+    db.session.add(usuario) 
+    db.session.commit()
+    flash('Dados atualizados com sucesso!')
+    return redirect(url_for('.recovery', id=id))
+
+@bp_usuarios.route('/delete/<int:id>', methods=['GET', 'POST'])
+def delete(id):
+    if id==0: 
+        flash('É preciso definir um usuário para ser excluído') 
+        return redirect(url_for('.recovery'))
+
+    if request.method == 'GET':
+        usuario = Usuario.query.get(id)
+        return render_template('usuarios_delete.html', usuario = usuario)
+    
+    if request.method == 'POST':
+        usuario = Usuario.query.get(id)
+        db.session.delete(usuario)
+        db.session.commit()
+        flash('Usuário excluído com sucesso')
+        return redirect(url_for('.recovery'))
+    #return redirect(url_for('/usuarios/recovery'))
